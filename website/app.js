b921eba91dbe688265563a08b74ff988337fc0cb
Emerson Espinoza <espinozer93@gmail.com>
2021-07-22 14:38:01 -0400

    add event listener

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 /* Global Variables */
 const baseUrl = "https://api.openweathermap.org/";
 const apiKey = "7292ab5b2616d42aed863e6989a0f3ca";
-const zipCode = document.getElementById("zip").value;
+const zipCode = document.getElementById("zip");
 const feelings = document.getElementById("feelings");
 
 const generate = document.getElementById("generate");
@@ -9,3 +9,10 @@ const generate = document.getElementById("generate");
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Add event listener
+generate.addEventListener("click", sendData);
+
+function sendData() {
+  console.log("test");
+}
