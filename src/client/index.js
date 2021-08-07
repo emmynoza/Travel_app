bdaa816e21409f6c59bad357ae44e9c0af120c7c
Emerson Espinoza <espinozer93@gmail.com>
2021-08-07 13:00:54 -0400

    set up a fixed bg

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,9 @@
 import logo from '../client/media/wanderlust-logo.png'
 import styles from './styles/style.scss'
+import base from './styles/_base.scss'
 
 export {
   logo,
-  styles
+  styles,
+  base
 }
